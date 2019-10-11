62436ad926c5f4e80a7080db1584eca8f65a5c39
Paulette S. Serrano <paulette.s.serrano@gmail.com>
2019-10-11 04:41:21 -0400

    New setup to make a fake chunk that invokes all NMM structgroups. Added the resulting master file and commented out pointers for problematic NMMs.

--- dsa_extras/nmm2dsa.py
+++ dsa_extras/nmm2dsa.py
@@ -2,8 +2,22 @@ from .nmm_common import name_stem
 from .nmm_group import emit_structgroup, read_nmm
 from .nmm_type import emit_types
 from dsa.parsing.file_parsing import output_file
+from dsa.parsing.line_parsing import INDENT
 from dsa.ui.entrypoint import entry_point, param
 import glob, os
+
+
+def _emit_master(entries):
+    yield [['align', '1'], ['count', '1']]
+    # The whole group is fake, having zero size.
+    # We set count=1 so disassembly only gets the pointers once.
+    yield []
+    yield [['MASTER']]
+    for name, offset in entries:
+        yield [
+            INDENT, ['Address'], [name],
+            ['referent', name], ['bias', hex(offset)]
+        ]
 
 
 @param('in_folder', 'source NMMs folder')
@@ -12,6 +26,7 @@ import glob, os
 @entry_point('NMM to DSA enumeration file converter')
 def nmm2dsa(in_folder, out_folder, out_types):
     type_data = {}
+    master_entries = []
     # Process each file first to accumulate type data,
     # then write out each output.
     all_group_data = [
@@ -25,4 +40,8 @@ def nmm2dsa(in_folder, out_folder, out_types):
         output_file(
             out_file, emit_structgroup(type_data, group_data), compact=True
         )
+        struct_data, chunk_offset, count, struct_size = group_data
+        master_entries.append((stem, chunk_offset))
+    master_file = os.path.join(out_folder, 'AllNMM.txt')
+    output_file(master_file, _emit_master(master_entries), compact=True)
     output_file(out_types, emit_types(type_data), compact=True)
