f5c1fdf478225843e4f3d8268f7c2ea8389fc021
Paulette S. Serrano <paulette.s.serrano@gmail.com>
2020-09-12 21:01:28 -0400

    set up demo

---
+++ demo/demo.bat
@@ -0,0 +1,15 @@
+@echo off
+pushd %~dp0
+mkdir fe7
+cd fe7
+rem dsd -l sys -p ..\..\dsa_extras\library -t fe7 "%FE7%" 0xb808ac:Text text.txt
+rem dsd -l sys -p ..\..\dsa_extras\library -t fe7 "%FE7%" 0:Events events.txt
+rem dsd -l sys -p ..\..\dsa_extras\library -t fe7 "%FE7%" 0:Misc result.txt
+cd ..
+mkdir fe8
+cd fe8
+dsd -l sys -p ..\..\dsa_extras\library -t fe8 "%FE8%" 0x15d48c:Text text.txt
+dsd -l sys -p ..\..\dsa_extras\library -t fe8 "%FE8%" 0:Events events.txt
+dsd -l sys -p ..\..\dsa_extras\library -t fe8 "%FE8%" 0:Misc misc.txt
+cd ..
+popd
